6079b1323eb2cd8473f73721e33ef7eb15a944f0
DannyV1992 <dava01cr@gmail.com>
2026-07-08 17:00:36 -0600

    Scroll top

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,15 +1,29 @@
 "use client";
 
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { useEffect, Suspense } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
-export default function ScrollToTop() {
+function ScrollResetter() {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
-    document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
-  }, [pathname]);
+    // setTimeout cede el hilo para que Next.js termine su scroll restoration
+    // antes de que nosotros lo pisemos con el reset al top
+    const id = setTimeout(() => {
+      document.documentElement.scrollTop = 0;
+      document.body.scrollTop = 0;
+    }, 0);
+    return () => clearTimeout(id);
+  }, [pathname, searchParams]);
 
   return null;
+}
+
+export default function ScrollToTop() {
+  return (
+    <Suspense>
+      <ScrollResetter />
+    </Suspense>
+  );
 }
