7e036e787e0a676b030a8d02134b569557a6d2b9
DannyV1992 <dava01cr@gmail.com>
2026-07-10 18:05:57 -0600

    Gtag ID

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_MEASUREMENT_ID = "G-GE5XQ9THJS";
+export const GA_MEASUREMENT_ID = "G-1F00E5F123";
 
 export function gtagEvent(action: string, params?: Record<string, string | number>) {
   if (typeof window !== "undefined" && window.gtag) {
